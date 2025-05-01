61fd0a03fca629824ac329b790b486c0cf115b88
XMD036 <khasanzhanovm@gmail.com>
2025-05-01 18:55:10 +0300

    Update script.js
    внесение функционала drop'ов

--- script.js
+++ script.js
@@ -15,4 +15,29 @@ document.addEventListener("DOMContentLoaded", function() {
         },
         loop: false,
       });
+
+
+
+      let drop_control = document.querySelectorAll('.drop_control');
+      // let drop_info = this.querySelector('.drop_info');
+      // let drop_title = this.querySelector('.drop_title');
+
+
+      drop_control.forEach(el => {
+        el.addEventListener('click', (e) => {
+          const self = e.currentTarget;
+          const drop = self.querySelector('.drop');
+          const drop_info = self.querySelector('.drop_info');
+          
+          self.classList.toggle('open');
+
+          if (self.classList.contains('open')) {
+            drop.setAttribute('aria-expended', true);
+            drop_info.setAttribute('aria-hidden', false);
+          } else {
+            drop.setAttribute('aria-expended', false);
+            drop_info.setAttribute('aria-hidden', true);
+          }
+        })
+      })
 })
